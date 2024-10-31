0e9ce2e52ea6cb1cf2ca65b90850473925b228a5
baouragh <you@example.com>
2024-10-31 05:12:26 +0100

    make ex06 clean

--- CPP01/ex06/main.cpp
+++ CPP01/ex06/main.cpp
@@ -9,37 +9,41 @@ int main(int ac, char **av)
         std::cout << "Args must be 2\n";
         return (1);
     }
-    std::string level = av[1];
+
+    Harl harly;
     std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    
+    std::string level = av[1];
     int i = 0;
-    Harl H;
 
     while (level != levels[i] && i < 4)
         i++;
+
     if (i == 4)
        i = -1;
+
     while( i < 4 )
     {
         switch (i)
         {
             case 0:
                 std::cout << "[ " << levels[i] << " ]" << std::endl;
-                H.complain(levels[i]);
+                harly.complain(levels[i]);
                 std::cout << std::endl;
                 break;
             case 1:
                 std::cout << "[ " << levels[i] << " ]" << std::endl;
-                H.complain(levels[i]);
+                harly.complain(levels[i]);
                 std::cout << std::endl;
                 break;
             case 2:
                 std::cout << "[ " << levels[i] << " ]" << std::endl;
-                H.complain(levels[i]);
+                harly.complain(levels[i]);
                 std::cout << std::endl;
                 break;
             case 3:
                 std::cout << "[ " << levels[i] << " ]" << std::endl;
-                H.complain(levels[i]);
+                harly.complain(levels[i]);
                 std::cout << std::endl;
                 break;
             default:
